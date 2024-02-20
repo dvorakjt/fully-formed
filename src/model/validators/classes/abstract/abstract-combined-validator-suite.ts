63b32a6324bab2d4615328f7ba172d1bee2f96ae
Joseph Dvorak <dvorakjt@gmail.com>
2024-02-20 02:34:33 -0500

    documented AbstractCombinedValidatorSuite

--- src/model/validators/classes/abstract/abstract-combined-validator-suite.ts
+++ src/model/validators/classes/abstract/abstract-combined-validator-suite.ts
@@ -1,5 +1,14 @@
 import type { CombinedValidatorSuiteResult } from '../../types';
 
+/**
+ * Provides synchronous and asynchronous validation for a given type of value.
+ */
 export abstract class AbstractCombinedValidatorSuite<T> {
+  /**
+   * Validates the provided value against both sync and async validators.
+   *
+   * @param value - The value to be validated.
+   * @returns An object containing a `syncResult` and possibly an `observableResult`.
+   */
   public abstract validate(value: T): CombinedValidatorSuiteResult<T>;
 }
