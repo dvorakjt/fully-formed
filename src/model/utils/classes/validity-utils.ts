9e46247561e5f41ae614aaca0cdd2657134b0b5e
Joseph Dvorak <dvorakjt@gmail.com>
2025-01-18 05:27:13 -0500

    added TSDoc comments for ValidityUtils

--- src/model/utils/classes/validity-utils.ts
+++ src/model/utils/classes/validity-utils.ts
@@ -7,12 +7,80 @@ type MinValidityOpts = {
   pruneUnvalidatedGroups: boolean;
 };
 
+/**
+ * A collection of utility methods for various common operations related to
+ * validity.
+ */
 export class ValidityUtils {
+  /**
+   * Accepts an entity such as a Field, the state of such an entity, or a
+   * value of type {@link Validity} and returns true if it is
+   * {@link Validity.Valid}.
+   *
+   * @param entityStateOrValidity - The entity, state or validity to evaluate.
+   * @returns `true` if the provided argument is valid, `false` otherwise.
+   *
+   * @example
+   * ```
+   * const field = new Field({
+   *   name: 'field',
+   *   defaultValue: '',
+   *   validatorTemplates: [
+   *     {
+   *       predicate: () => false
+   *     }
+   *   ]
+   * });
+   *
+   * ValidityUtils.isValid(field); // false
+   *
+   * const state : ValidatedState<string> = {
+   *   value: '',
+   *   validity: Validity.Pending
+   * };
+   *
+   * ValidityUtils.isValid(state); // false
+   *
+   * ValidityUtils.isValid(Validity.Caution); // false
+   *
+   * ValidityUtils.isValid(Validity.Valid); // true
+   * ```
+   */
   public static isValid(entityStateOrValidity: EntityStateOrValidity): boolean {
     const validity = this.toValidity(entityStateOrValidity);
     return validity === Validity.Valid;
   }
 
+  /**
+   * Accepts an entity such as a Field, the state of such an entity, or a
+   * value of type {@link Validity} and returns true if it its validity is
+   * {@link Validity.Caution}.
+   *
+   * @param entityStateOrValidity - The entity, state or validity to evaluate.
+   * @returns `true` if the validity of the argument is
+   * {@link Validity.Caution}, `false` otherwise.
+   *
+   * @example
+   * ```
+   * const field = new Field({
+   *   name: 'field',
+   *   defaultValue: ''
+   * });
+   *
+   * ValidityUtils.isCaution(field); // false
+   *
+   * const state : ValidatedState<string> = {
+   *   value: '',
+   *   validity: Validity.Pending
+   * };
+   *
+   * ValidityUtils.isCaution(state); // false
+   *
+   * ValidityUtils.isCaution(Validity.Invalid); // false
+   *
+   * ValidityUtils.isCaution(Validity.Caution); // true
+   * ```
+   */
   public static isCaution(
     entityStateOrValidity: EntityStateOrValidity,
   ): boolean {
@@ -20,6 +88,41 @@ export class ValidityUtils {
     return validity === Validity.Caution;
   }
 
+  /**
+   * Accepts an entity such as a Field, the state of such an entity, or a
+   * value of type {@link Validity} and returns true if it its validity is
+   * {@link Validity.Valid} or {@link Validity.Caution}.
+   *
+   * @param entityStateOrValidity - The entity, state or validity to evaluate.
+   * @returns `true` if the validity of the argument is
+   * {@link Validity.Valid} or {@link Validity.Caution}, `false` otherwise.
+   *
+   * @example
+   * ```
+   * const field = new Field({
+   *   name: 'field',
+   *   defaultValue: '',
+   *   validatorTemplates: [
+   *     {
+   *       predicate: () => false
+   *     }
+   *   ]
+   * });
+   *
+   * ValidityUtils.isValidOrCaution(field); // false
+   *
+   * const state : ValidatedState<string> = {
+   *   value: '',
+   *   validity: Validity.Pending
+   * };
+   *
+   * ValidityUtils.isValidOrCaution(state); // false
+   *
+   * ValidityUtils.isValidOrCaution(Validity.Caution); // true
+   *
+   * ValidityUtils.isValidOrCaution(Validity.Valid); // true
+   * ```
+   */
   public static isValidOrCaution(
     entityStateOrValidity: EntityStateOrValidity,
   ): boolean {
@@ -27,6 +130,36 @@ export class ValidityUtils {
     return validity === Validity.Valid || validity === Validity.Caution;
   }
 
+  /**
+   * Accepts an entity such as a Field, the state of such an entity, or a
+   * value of type {@link Validity} and returns true if it its validity is
+   * {@link Validity.Pending}.
+   *
+   * @param entityStateOrValidity - The entity, state or validity to evaluate.
+   * @returns `true` if the validity of the argument is
+   * {@link Validity.Pending}, `false` otherwise.
+   *
+   * @example
+   * ```
+   * const field = new Field({
+   *   name: 'field',
+   *   defaultValue: ''
+   * });
+   *
+   * ValidityUtils.isPending(field); // false
+   *
+   * const state : ValidatedState<string> = {
+   *   value: '',
+   *   validity: Validity.Caution
+   * };
+   *
+   * ValidityUtils.isPending(state); // false
+   *
+   * ValidityUtils.isPending(Validity.Valid); // false
+   *
+   * ValidityUtils.isPending(Validity.Pending); // true
+   * ```
+   */
   public static isPending(
     entityStateOrValidity: EntityStateOrValidity,
   ): boolean {
@@ -34,6 +167,36 @@ export class ValidityUtils {
     return validity === Validity.Pending;
   }
 
+  /**
+   * Accepts an entity such as a Field, the state of such an entity, or a
+   * value of type {@link Validity} and returns true if it its validity is
+   * {@link Validity.Invalid}.
+   *
+   * @param entityStateOrValidity - The entity, state or validity to evaluate.
+   * @returns `true` if the validity of the argument is
+   * {@link Validity.Invalid}, `false` otherwise.
+   *
+   * @example
+   * ```
+   * const field = new Field({
+   *   name: 'field',
+   *   defaultValue: ''
+   * });
+   *
+   * ValidityUtils.isInvalid(field); // false
+   *
+   * const state : ValidatedState<string> = {
+   *   value: '',
+   *   validity: Validity.Pending
+   * };
+   *
+   * ValidityUtils.isInvalid(state); // false
+   *
+   * ValidityUtils.isInvalid(Validity.Caution); // false
+   *
+   * ValidityUtils.isInvalid(Validity.Invalid); // true
+   * ```
+   */
   public static isInvalid(
     entityStateOrValidity: EntityStateOrValidity,
   ): boolean {
@@ -41,6 +204,22 @@ export class ValidityUtils {
     return validity === Validity.Invalid;
   }
 
+  /**
+   * Accepts an array of entities (such as Fields), states, and/or validities
+   * and returns the minimum validity found in the array.
+   *
+   * @param entitiesStatesOrValidities - An array of entities, states, and/or
+   * validities whose minimum validity will be returned.
+   *
+   * @param opts - A configuration object which enables the user to define
+   * whether unvalidated groups should be ignored.
+   *
+   * @returns The minimum {@link Validity} of the array.
+   *
+   * @remarks
+   * The different possible validities relate to each other as follows:
+   * {@link Validity.Invalid} \< {@link Validity.Pending} \< {@link Validity.Caution} \< {@link Validity.Valid}
+   */
   public static minValidity(
     entitiesStatesOrValidities:
       | EntityStateOrValidity[]
